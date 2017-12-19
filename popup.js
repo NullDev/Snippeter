950def13062d5701cf1504c9c1ce4df8eb1ba136
NullDev (Shadow) <NL-Dev@yandex.com>
2017-12-19 13:35:51 +0100

    Added handler for snippet counts

---
+++ popup.js
@@ -0,0 +1,19 @@
+"use strict";
+var storage = chrome.storage.local;
+
+////////////////////////////////
+//----------------------------//
+// Copyright (c) 2017 NullDev //
+//----------------------------//
+////////////////////////////////
+
+var noop = () => {};
+
+chrome.extension.sendMessage({}, function(response) { noop(); });
+
+$(document).ready(function(){
+    chrome.runtime.sendMessage({ type: "init"}, function(res){ 
+        storage.get("count", function(i){ $("#count").text((i.count ? i.count : "0")); });
+        return true;
+    });
+});
