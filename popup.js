0fa94230f51e2d6391aa8506c85ded30190261af
NullDev (Shadow) <NL-Dev@yandex.com>
2017-12-19 14:13:50 +0100

    Fix for "snippet"/"snippets" (plural)

--- popup.js
+++ popup.js
@@ -13,7 +13,10 @@ chrome.extension.sendMessage({}, function(response) { noop(); });
 
 $(document).ready(function(){
     chrome.runtime.sendMessage({ type: "init"}, function(res){ 
-        storage.get("count", function(i){ $("#count").text((i.count ? i.count : "0")); });
+        storage.get("count", function(i){ 
+            $("#count").text((i.count ? i.count : "0"));
+            $("#plural").text((i.count == 1 ? "" : "s")); 
+        });
         return true;
     });
 });
