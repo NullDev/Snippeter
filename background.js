242776c73ea1fa834d970fd645aea5de024192fe
NullDev (Shadow) <NL-Dev@yandex.com>
2017-12-21 09:58:39 +0100

    Added POST request logic

--- background.js
+++ background.js
@@ -7,6 +7,8 @@ var storage = chrome.storage.local;
 //----------------------------//
 ////////////////////////////////
 
+var uri = "https://jsfiddle.net/";
+
 var initData = function(){
     chrome.tabs.query({ active: true, currentWindow: true }, function(arr){
         var activeTab = arr[0];
@@ -18,21 +20,33 @@ var initData = function(){
 }
 
 var jsfRequest = function(resObj){
-    var js   = resObj.js;
-    var css  = resObj.css;
-    var html = resObj.html;
+    var js   = (resObj.js   ? resObj.js   : "");
+    var css  = (resObj.css  ? resObj.css  : "");
+    var html = (resObj.html ? resObj.html : "");
 
-    var uri = "https://jsfiddle.net/";
     chrome.tabs.create({ url: uri });
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(arr){
         var activeTab = arr[0];
         var url = (activeTab.url).toLowerCase();
 
-        if (/(jsfiddle\.net)/gi.test(url)) storage.set({ js: js, css: css, html: html });
+        if (/(jsfiddle\.net)/gi.test(url)) postReq("https://jsfiddle.net/api/post/library/pure/", { js: js, css: css, html: html });
         return true;
     });
 };
+
+function postReq(url, data) {
+    chrome.tabs.create({ url: chrome.runtime.getURL("post.html") }, function(tab){
+        var handler = function(tabId, changeInfo){
+            if(tabId === tab.id && changeInfo.status === "complete"){
+                chrome.tabs.onUpdated.removeListener(handler);
+                chrome.tabs.sendMessage(tabId, { url: url, data: data });
+            }
+        }
+        chrome.tabs.onUpdated.addListener(handler);
+        chrome.tabs.sendMessage(tab.id, { url: url, data: data });
+    });  
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     switch(request.type){
