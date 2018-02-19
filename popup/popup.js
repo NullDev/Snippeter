73d4535145ab79194ee23327936dedd2061ed6c7
NullDev (Shadow) <NL-Dev@yandex.com>
2018-02-19 11:14:25 +0100

    Fixed initial switch state

--- popup/popup.js
+++ popup/popup.js
@@ -11,13 +11,18 @@ var noop = () => {};
 
 chrome.extension.sendMessage({}, function(response) { noop(); });
 
+function isset(obj){ return !!(obj && obj !== null && (typeof obj === 'string' && obj !== "")); }
+
 $(document).ready(function(){
     chrome.runtime.sendMessage({ type: "init"}, function(res){ 
         storage.get("count", function(i){ 
             $("#count").text((i.count ? i.count : "0"));
             $("#plural").text((i.count == 1 ? "" : "s")); 
         });
-        storage.get("nstate", function(i){ $(".switch-box-input").prop("checked", i.nstate); });
+        storage.get("nstate", function(i){ 
+            if (!isset(i.nstate)) storage.set({ nstate: true });
+            $(".switch-box-input").prop("checked", i.nstate); 
+        });
         return true;
     });
 
